170fb9fe08b80a215f536d6425b3651b40c1b946
laimison <laimison1@gmail.com>
2019-09-01 16:56:58 +0100

    csv bug fixed & moved track list files to the project's dir

--- music_matcher.sh
+++ music_matcher.sh
@@ -3,7 +3,29 @@
 # Do not continue a shell script if failed
 # set -e
 
+# Given playlist
+TRACK_CANDIDATE_LIST=tmp/given.tracks
+TRACK_CANDIDATE_LIST_CORRECTED=tmp/given.tracks.corrected
+
+# Local music
 SEARCH_FOR_MUSIC_DIR="${HOME}/Music"
+TRACK_LOCAL_LIST=tmp/tracks.all
+TRACK_LOCAL_LIST_TAGGED=tmp/tracks.all.tagged
+
+# The final result what I don't have
+TRACKS_THAT_DONT_EXIST_LOCALLY=tmp/tracks.that.dont.exist.locally
+
+if ! ls music_matcher.sh 2>/dev/null | grep -q music_matcher.sh
+then
+  echo "You are probably not in the right directory"
+  exit 1
+fi
+
+if ! which ffprobe
+then
+  echo "Cannot find ffprobe binary"
+  exit 1
+fi
 
 open "http://www.playlist-converter.net"
 
@@ -15,52 +37,53 @@ read csv
 
 if echo $csv | grep -q [a-zA-Z0-9]
 then
-  iconv -f utf-8 -t ascii ~/Downloads/Liked\ from\ Radio.csv > /tmp/tracks.converted.encoding.csv
-  cat /tmp/tracks.converted.encoding.csv | tail -n +2 | tr '",' ' ' > /tmp/tracks
+  iconv -f utf-8 -t ascii "${csv}" > /tmp/tracks.converted.encoding.csv.tmp
+  cat /tmp/tracks.converted.encoding.csv.tmp | tail -n +2 | tr '",' ' ' > $TRACK_CANDIDATE_LIST
 fi
 
 # Check if I have tracks file to start with
-if ! ls /tmp/tracks >/dev/null 2>/dev/null; then echo "Please create /tmp/tracks with your tracks list, e.g. The Glitch Mob Animus Vox, The Glitch Mob - Animus Vox.mp3, etc."; fi
+if ! ls $TRACK_CANDIDATE_LIST >/dev/null 2>/dev/null; then echo "Please create $TRACK_CANDIDATE_LIST with your tracks list, e.g. The Glitch Mob Animus Vox, The Glitch Mob - Animus Vox.mp3, etc."; fi
 
 # Remove unwanted characters and make list unique
 # Cut ' -', '[anything-here]', '&', '.', 'original mix'
-cat /tmp/tracks | sed -e "s| -||g" | sed -e "s|\[.*\]||g" | sed -e "s|\&||g" | sed -e "s|\.||g" | sed -e "s|[Oo]riginal [Mm]ix||g" | awk '!uniq[substr($0, 12, 8)]++' > /tmp/tracks.corrected
+cat $TRACK_CANDIDATE_LIST | sed -e "s| -||g" | sed -e "s|\[.*\]||g" | sed -e "s|\&||g" | sed -e "s|\.||g" | sed -e "s|[Oo]riginal [Mm]ix||g" | awk '!uniq[substr($0, 12, 8)]++' > $TRACK_CANDIDATE_LIST_CORRECTED
 
+################################################# UPDATE DB ############################################
 echo "Find all tracks - artist ||| title ||| file name"
 echo "Update tracks list? Type in 'yes' for update, press enter if you have updated recently"
 read input
 
 if echo $input | grep -i ^yes$
 then
-  find "${SEARCH_FOR_MUSIC_DIR}" -name '*.mp3' > /tmp/tracks.all.untagged
+  find "${SEARCH_FOR_MUSIC_DIR}" -name '*.mp3' > $TRACK_LOCAL_LIST
 
-  > /tmp/tracks.all
+  > $TRACK_LOCAL_LIST_TAGGED
   while IFS= read -r track; do
     ffprobe "$track" 2> /tmp/tracks.ffprobe
     artist=`cat /tmp/tracks.ffprobe | grep '^[[:space:]]\+artist[[:space:]]\+\:[[:space:]]\+' | sed -e s/artist.*\://g | head -n 1`
     title=`cat /tmp/tracks.ffprobe | grep '^[[:space:]]\+title[[:space:]]\+\:[[:space:]]\+' | sed -e s/title.*\://g | head -n 1`
 
-    # echo "$artist ||| $title ||| `basename \"$track\"` ||| $track" | tee -a /tmp/tracks.all
-    echo "$artist ||| $title ||| `basename \"$track\"`" | tee -a /tmp/tracks.all
+    # echo "$artist ||| $title ||| `basename \"$track\"` ||| $track" | tee -a $TRACK_LOCAL_LIST_TAGGED
+    echo "$artist ||| $title ||| `basename \"$track\"`" | tee -a $TRACK_LOCAL_LIST_TAGGED
     if [ "$?" != "0" ]
     then
       echo "Issue with the script"
       exit 1
     fi
-  done < /tmp/tracks.all.untagged
+  done < $TRACK_LOCAL_LIST
 fi
 
-echo "In total found `wc -l /tmp/tracks.all | awk -F ' ' '{print $1}'` tracks on your machine. You have given `wc -l /tmp/tracks.corrected | awk -F ' ' '{print $1}'` tracks. Continuing to find tracks that do not exist on your machine in 3 seconds ..."
+echo "In total found `wc -l $TRACK_LOCAL_LIST_TAGGED | awk -F ' ' '{print $1}'` tracks on your machine. You have given `wc -l $TRACK_CANDIDATE_LIST_CORRECTED | awk -F ' ' '{print $1}'` tracks. Continuing to find tracks that do not exist on your machine in 3 seconds ..."
 sleep 3
+#########################################################################################################
 
 > /tmp/tracks.found
-> /tmp/tracks.not.found
-
+> $TRACKS_THAT_DONT_EXIST_LOCALLY
 # Go through given track list
 while IFS='' read -r track
 do
   # Copy the file which contains all tracks to temporary file
-  cat /tmp/tracks.all > /tmp/tracks.temp
+  cat $TRACK_LOCAL_LIST_TAGGED > /tmp/tracks.temp
   > /tmp/tracks.temp.2
 
   for word in $track
@@ -79,10 +102,10 @@ do
     echo "Found: `cat /tmp/tracks.temp | head -n 1`" | tee -a /tmp/tracks.found
   else
     # From DOS to Linux
-    echo "$track" | tr -d '\015' >> /tmp/tracks.not.found
+    echo "$track" | tr -d '\015' >> $TRACKS_THAT_DONT_EXIST_LOCALLY
   fi
-done < /tmp/tracks.corrected
+done < $TRACK_CANDIDATE_LIST_CORRECTED
 
 echo
 echo "Tracks not found:"
-cat /tmp/tracks.not.found
+cat $TRACKS_THAT_DONT_EXIST_LOCALLY
